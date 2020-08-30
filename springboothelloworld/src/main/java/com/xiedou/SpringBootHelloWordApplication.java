5fd9d58692b3ca2897cd6a45e29c1a234ecfac16
xie-doudou <2523833085@qq.com>
2020-08-30 17:26:51 +0800

    第二次提交

--- springboothelloworld/src/main/java/com/xiedou/SpringBootHelloWordApplication.java
+++ springboothelloworld/src/main/java/com/xiedou/SpringBootHelloWordApplication.java
@@ -8,6 +8,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class SpringBootHelloWordApplication {
 
     public static void main(String[] args) {
+
         SpringApplication.run(SpringBootHelloWordApplication.class, args);
     }
 }
